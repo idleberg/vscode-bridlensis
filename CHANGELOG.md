51824250363fc8523ccca50584d9a77b9e7a8e80
idleberg <git@idleberg.com>
2020-10-23 23:29:16 +0200

    version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v1.2.0 [#](https://github.com/idleberg/vscode-bridlensis/releases/tag/1.2.0)
+
+- add support for variable substition when reading config
+
 # v1.1.1 [#](https://github.com/idleberg/vscode-bridlensis/releases/tag/1.1.1)
 
 - fix: add missing extension dependency
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import * as vscode from 'vscode';
 // Load package components
 import { transpile } from './transpiler';
 
-const activate = (context: vscode.ExtensionContext) => {
+async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerTextEditorCommand('extension.bridlensis.transpile', (editor) => {
       return transpile();
     })
   );
-};
+}
 
 export { activate };
